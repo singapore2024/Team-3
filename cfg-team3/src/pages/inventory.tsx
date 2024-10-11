0a7730da1470d27912f91c7324a904a0ccaf26b5
Zoe Ang <za.zoeang@gmail.com>
2024-10-11 14:53:25 +0800

    Change sizing

--- cfg-team3/src/pages/inventory.tsx
+++ cfg-team3/src/pages/inventory.tsx
@@ -7,12 +7,12 @@ export default function Inventory() {
       <WithSubnavigation />
       <div className="flex flex-col p-7">
         <div className="flex flex-row justify-between items-center font-semibold">
-          <h1 className="text-6xl">Inventory</h1>
+          <h1 className="text-5xl">Inventory</h1>
           <div className="flex flex-row gap-10">
-            <button className="bg-slate-400 p-5 text-4xl rounded-lg">
+            <button className="bg-slate-400 p-5 text-3xl rounded-lg">
               Add Item
             </button>
-            <button className="bg-slate-400 p-5 text-4xl rounded-lg">
+            <button className="bg-slate-400 p-5 text-3xl rounded-lg">
               Remove Item
             </button>
           </div>
